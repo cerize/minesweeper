f3be047bfa40e154f9f337d112fbfa84cf8e09a5
Cerize Santos <cerize@Cerizes-MBP-2.hitronhub.home>
2017-04-16 14:33:52 -0700

    add background

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Game from './Game';
-
-
-
 
 class App extends Component {
     render() {
         return (  
-            <Router className="App">
-                <div>
+            <Router>
+                <div className="App">
                     <div className="App-header">
-                        <img src={logo} className="App-logo" alt="logo" />
                         <h2>Meta Minesweeper</h2>
                     </div>
-                    <p className="App-intro">
-                        To get started, edit <code>src/App.js</code> and save to reload.
-                    </p>
-            
-                    
-                    
-               
-
                     <Route exact path="/" component={Game} />
                     <Route path="/:id" component={Game} />
 
